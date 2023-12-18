d854ba0e5d7689a0c8062d310f01539388ebdae1
tuanzaisyah <tuannurulaisyah@gmail.com>
2023-12-18 12:18:31 +0800

    add link to home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="w-screen h-20 px-4 flex items-center absolute">
-      <img src="../../assets/img/logo.png" alt="logo" className="w-36 h-auto" />
+    <div className="w-screen h-20 md:h-28 px-4 md:px-10 flex items-center absolute">
+      <Link to={"/"}>
+        <img
+          src="../../assets/img/logo.png"
+          alt="logo"
+          className="w-36 h-auto"
+        />
+      </Link>
     </div>
   );
 };
