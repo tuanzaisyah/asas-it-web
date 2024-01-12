ff6febe599efa6e0b57ba00c71c3d0d3102ad38f
tuanzaisyah <tuannurulaisyah@gmail.com>
2024-01-12 23:45:35 +0800

    update quiz questions

--- src/data.js
+++ src/data.js
@@ -28,6 +28,47 @@ export const note = [
       "../../assets/notes/note2/4.png",
       "../../assets/notes/note2/5.png",
     ],
+    quiz: [
+      {
+        question:
+          "Yang manakah antara berikut merupakan penggunaan teknik leraian dalam kehidupan?",
+        options: [
+          "Apakah jenis ubat gigi yang digunakan?",
+          "Apakah jenis berus gigi yang digunakan?",
+          "Berapa kalikah gosokan untuk memberus gigi di sebelah kiri dan kanan masing-masing?",
+          "Semua jawapan di atas",
+        ],
+        correctOptionIndex: 4,
+      },
+      {
+        question: "Teknik leraian melibatkan... ",
+        options: [
+          "pemecahan suatu masalah atau sistem yang kompleks kepada bahagian-bahagian kecil bagi memudahkan pemahaman dan penyelesaian",
+          "Penguraian masalah kepada beberapa bahagian bagi memudahkan menyelesaikan masalah",
+          "Pembahagian jenis leraian yang memudahkan pemahaman",
+          "Penyatuan beberapa jenis leraian bagi tujuan memudahkan pemahaman",
+        ],
+        correctOptionIndex: 1,
+      },
+      {
+        question:
+          "Andaikan anda ialah seorang juru landskap. Syarikat anda ditugaskan untuk menyiapkan sebuah taman permainan di sebuah kawasan di luar bandar. Anda dipilih untuk mengkaji dan menganggarkan kos yang diperlukan bagi menyiapkan taman permainan.",
+        options: ["Betul", "Salah"],
+        correctOptionIndex: 1,
+      },
+      {
+        question:
+          "Dalam satu soalan Matematik berkaitan pecahan, Rina memecahkan soalan kepada beberapa langkah sebelum menyelesaikannya.",
+        options: ["Betul", "Salah"],
+        correctOptionIndex: 1,
+      },
+      {
+        question:
+          "Ketika membaca buku cerita, Karina hanya membaca bahagian rumusan di akhir sekali untuk mendapatkan isi pentingnya",
+        options: ["Betul", "Salah"],
+        correctOptionIndex: 2,
+      },
+    ],
   },
   {
     id: 3,
@@ -43,6 +84,55 @@ export const note = [
       "../../assets/notes/note3/5.png",
       "../../assets/notes/note3/6.png",
     ],
+    quiz: [
+      {
+        question: "Apakah yang dimaksudkan dengan pengecaman corak?",
+        options: [
+          "Menggabungkan masalah yang kompleks daripada pecahan-pecahan kecil",
+          "Memecahkan masalah yang kompleks kepada pecahan-pecahan yang lebih kecil",
+          "Mencari perbezaan yang terdapat antara masalah dan dalam masalah",
+          "Mencari kesamaan yang terdapat antara masalah dan dalam masalah",
+        ],
+        correctOptionIndex: 4,
+      },
+      {
+        question: "Manakah antara yang berikut merujuk kepada corak?",
+        options: [
+          "Semua kereta mempunyai roda",
+          "Kereta saya warna biru gelap dan berkilat",
+          "Kereta saya di import daripada Jepun",
+          "Kereta kawan saya mempunyai pemacu MP3",
+        ],
+        correctOptionIndex: 1,
+      },
+      {
+        question: "Manakah antara yang berikut TIDAK merujuk kepada corak?",
+        options: [
+          "Semua kucing mempunyai ekor",
+          " Kucing saya boleh berenang",
+          "Kucing boleh makan baja khas kucing",
+          "Kucing kami suka bersiar-siar",
+        ],
+        correctOptionIndex: 2,
+      },
+      {
+        question:
+          "Manakah antara situasi berikut TIDAK merujuk kepada teknik Pengecaman corak?",
+        options: [
+          "Polis mengenalpasti kumpulan perompak yang melakukan jenayah rompakan bersenjata berdasarkan modus operandi mereka sebelum ini",
+          "Guru Sains menyatakan fungsi setiap bahagian dalam satu tumbuhan",
+          "Guru meminta anda membeli pen biru. Anda mengabaikan jenama atau jenis pen perlu dibeli",
+          "Saya meneruskan perjalanan apabila melihat lampu isyarat hijau, walaupun pada ketika itu saya baru sahaja berada di United Kingdom kerana saya tahu semua negara mengamalkan peraturan yang sama",
+        ],
+        correctOptionIndex: 3,
+      },
+      {
+        question:
+          "Komputer polis boleh mengesan penjenayah dengan menggunakan sistem yang mengenal pasti cap jari",
+        options: ["Peniskalaan", "Leraian", "Pengecaman Corak", "Pengitlakan"],
+        correctOptionIndex: 3,
+      },
+    ],
   },
   {
     id: 4,
@@ -57,6 +147,52 @@ export const note = [
       "../../assets/notes/note4/4.png",
       "../../assets/notes/note4/5.png",
       "../../assets/notes/note4/6.png",
+    ],
+    quiz: [
+      {
+        question: "Aspek-aspek penting perlu dikenalpasti____________",
+        options: [
+          "Selepas membuat keputusan",
+          "Semasa membuat keputusan",
+          "Sebelum membuat keputusan",
+        ],
+        correctOptionIndex: 3,
+      },
+      {
+        question:
+          "Aspek penting perlu diambil kira sebelum membuat keputusan kerana",
+        options: [
+          "Mempengaruhi hasil atau output yang dikehendaki",
+          "Mudah",
+          "Memudahkan",
+        ],
+        correctOptionIndex: 1,
+      },
+      {
+        question:
+          "Komputer akan mencari laluan paling kurang __________ semasa proses menghantar data melalui talian",
+        options: ["sempit", "kekangan atau halangan", "tarikan"],
+        correctOptionIndex: 2,
+      },
+      {
+        question: "Aspek-aspek penting membolehkan kita___________",
+        options: [
+          "Membuat pilihan terbaik",
+          "Memenuhi syarat",
+          "Mudah membuat kerja",
+        ],
+        correctOptionIndex: 1,
+      },
+      {
+        question:
+          "Pemikiran komputasional iaitu kemahiran membuat keputusan berdasrkan aspek penting dapat digunakan dalam kehidupan seharian seperti_____________",
+        options: [
+          "Bermain game",
+          "Penggunaan GPS dalam menentukan laluan terdekat",
+          "Membuat kerja rumah",
+        ],
+        correctOptionIndex: 2,
+      },
     ],
   },
   {
@@ -80,3 +216,53 @@ export const note = [
     ],
   },
 ];
+
+// export const quiz = [
+//   {
+//     question: "Apakah yang dimaksudkan dengan pengecaman corak?",
+//     options: [
+//       "Menggabungkan masalah yang kompleks daripada pecahan-pecahan kecil",
+//       "Memecahkan masalah yang kompleks kepada pecahan-pecahan yang lebih kecil",
+//       "Mencari perbezaan yang terdapat antara masalah dan dalam masalah",
+//       "Mencari kesamaan yang terdapat antara masalah dan dalam masalah",
+//     ],
+//     correctOptionIndex: 4,
+//   },
+//   {
+//     question: "Manakah antara yang berikut merujuk kepada corak?",
+//     options: [
+//       "Semua kereta mempunyai roda",
+//       "Kereta saya warna biru gelap dan berkilat",
+//       "Kereta saya di import daripada Jepun",
+//       "Kereta kawan saya mempunyai pemacu MP3",
+//     ],
+//     correctOptionIndex: 1,
+//   },
+//   {
+//     question: "Manakah antara yang berikut TIDAK merujuk kepada corak?",
+//     options: [
+//       "Semua kucing mempunyai ekor",
+//       " Kucing saya boleh berenang",
+//       "Kucing boleh makan baja khas kucing",
+//       "Kucing kami suka bersiar-siar",
+//     ],
+//     correctOptionIndex: 2,
+//   },
+//   {
+//     question:
+//       "Manakah antara situasi berikut TIDAK merujuk kepada teknik Pengecaman corak?",
+//     options: [
+//       "Polis mengenalpasti kumpulan perompak yang melakukan jenayah rompakan bersenjata berdasarkan modus operandi mereka sebelum ini",
+//       "Guru Sains menyatakan fungsi setiap bahagian dalam satu tumbuhan",
+//       "Guru meminta anda membeli pen biru. Anda mengabaikan jenama atau jenis pen perlu dibeli",
+//       "Saya meneruskan perjalanan apabila melihat lampu isyarat hijau, walaupun pada ketika itu saya baru sahaja berada di United Kingdom kerana saya tahu semua negara mengamalkan peraturan yang sama",
+//     ],
+//     correctOptionIndex: 3,
+//   },
+//   {
+//     question:
+//       "Komputer polis boleh mengesan penjenayah dengan menggunakan sistem yang mengenal pasti cap jari",
+//     options: ["Peniskalaan", "Leraian", "Pengecaman Corak", "Pengitlakan"],
+//     correctOptionIndex: 3,
+//   },
+// ];
