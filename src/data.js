75f14d2440dbb148370e1fcee0a027a331b07693
tuanzaisyah <tuannurulaisyah@gmail.com>
2024-01-13 16:04:28 +0800

    update nota desc

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const note = [
   {
     id: 1,
     topic: "1.1.1 Teknik dalam Pemikiran Komputasional",
-    desc: "desc1",
+    desc: "Menghuraikan tentang proses pemikiran komputasional dan teknik-teknik di dalam pemikiran komputasional",
     video: "../../assets/videos/sample.mp4",
     note: "../../assets/notes/note1/nota1.pdf",
     images: [
@@ -62,8 +62,8 @@ export const note = [
     id: 2,
     topic:
       "1.1.2 Penggunaan Teknik Leraian dan Menentukan Langkah Secara Tertib",
-    desc: "desc2",
-    video: "../../assets/videos/sample2.mp4",
+    desc: "Teknik Leraina membolehkan kita memahami sesuatu proses kerja dengan lebih mudah dan membantu kita memberi penerangan dengan lebih jelas",
+    video: "../../assets/videos/sample.mp4",
     note: "../../assets/notes/note2/nota2.pdf",
     images: [
       "../../assets/notes/note2/1.png",
@@ -116,9 +116,9 @@ export const note = [
   },
   {
     id: 3,
-    topic: "1.1.3 Pengecaman Coak dalam Sesuatu Situasi",
-    desc: "desc3",
-    video: "../../assets/videos/sample.mp4",
+    topic: "1.1.3 Pengecaman Corak dalam Sesuatu Situasi",
+    desc: "Teknik Pengecaman Corak digunakan untuk menyelesaikan masalah melalui pengecaman persamaan corak",
+    video: "../../assets/videos/video3.mp4",
     note: "../../assets/notes/note3/nota3.pdf",
     images: [
       "../../assets/notes/note3/1.png",
@@ -181,8 +181,8 @@ export const note = [
   {
     id: 4,
     topic: "1.1.4 Membuat Keputusan Berdasarkan Aspek Penting",
-    desc: "desc4",
-    video: "../../assets/videos/sample2.mp4",
+    desc: "Aspek penting perlu dikenalpasti untuk mengambil kira pilihan yang ada kerana ianya mempengaruhi hasil atau output yang dikehendaki",
+    video: "../../assets/videos/sample.mp4",
     note: "../../assets/notes/note4/nota4.pdf",
     images: [
       "../../assets/notes/note4/1.png",
@@ -242,7 +242,7 @@ export const note = [
   {
     id: 5,
     topic: "1.1.5 Ciri-Ciri Kesamaan dalam Sesuatu Permasalahan",
-    desc: "desc5",
+    desc: "Mengenal pasti kesamaan atau perbezaan dalam sesuatu masalah bagi membantu mereka bentuk algoritma untuk menyelesaikan sesuatu masalah",
     video: "../../assets/videos/sample.mp4",
     note: "../../assets/notes/note5/nota5.pdf",
     images: [
@@ -261,52 +261,30 @@ export const note = [
   },
 ];
 
-// export const quiz = [
-//   {
-//     question: "Apakah yang dimaksudkan dengan pengecaman corak?",
-//     options: [
-//       "Menggabungkan masalah yang kompleks daripada pecahan-pecahan kecil",
-//       "Memecahkan masalah yang kompleks kepada pecahan-pecahan yang lebih kecil",
-//       "Mencari perbezaan yang terdapat antara masalah dan dalam masalah",
-//       "Mencari kesamaan yang terdapat antara masalah dan dalam masalah",
-//     ],
-//     correctOptionIndex: 4,
-//   },
-//   {
-//     question: "Manakah antara yang berikut merujuk kepada corak?",
-//     options: [
-//       "Semua kereta mempunyai roda",
-//       "Kereta saya warna biru gelap dan berkilat",
-//       "Kereta saya di import daripada Jepun",
-//       "Kereta kawan saya mempunyai pemacu MP3",
-//     ],
-//     correctOptionIndex: 1,
-//   },
-//   {
-//     question: "Manakah antara yang berikut TIDAK merujuk kepada corak?",
-//     options: [
-//       "Semua kucing mempunyai ekor",
-//       " Kucing saya boleh berenang",
-//       "Kucing boleh makan baja khas kucing",
-//       "Kucing kami suka bersiar-siar",
-//     ],
-//     correctOptionIndex: 2,
-//   },
-//   {
-//     question:
-//       "Manakah antara situasi berikut TIDAK merujuk kepada teknik Pengecaman corak?",
-//     options: [
-//       "Polis mengenalpasti kumpulan perompak yang melakukan jenayah rompakan bersenjata berdasarkan modus operandi mereka sebelum ini",
-//       "Guru Sains menyatakan fungsi setiap bahagian dalam satu tumbuhan",
-//       "Guru meminta anda membeli pen biru. Anda mengabaikan jenama atau jenis pen perlu dibeli",
-//       "Saya meneruskan perjalanan apabila melihat lampu isyarat hijau, walaupun pada ketika itu saya baru sahaja berada di United Kingdom kerana saya tahu semua negara mengamalkan peraturan yang sama",
-//     ],
-//     correctOptionIndex: 3,
-//   },
-//   {
-//     question:
-//       "Komputer polis boleh mengesan penjenayah dengan menggunakan sistem yang mengenal pasti cap jari",
-//     options: ["Peniskalaan", "Leraian", "Pengecaman Corak", "Pengitlakan"],
-//     correctOptionIndex: 3,
-//   },
-// ];
+export const team = [
+  {
+    id: 1,
+    img: "../../assets/img/person1.jpeg",
+    name: "A'isyah",
+  },
+  {
+    id: 2,
+    img: "../../assets/img/person2.jpg",
+    name: "Zaimizan",
+  },
+  {
+    id: 3,
+    img: "../../assets/img/person3.png",
+    name: "Elawatun",
+  },
+  {
+    id: 4,
+    img: "../../assets/img/person4.png",
+    name: "Leeny",
+  },
+  {
+    id: 5,
+    img: "../../assets/img/person5.png",
+    name: "Charirn",
+  },
+];
